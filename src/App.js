009a6ed9cc78dddbe8395b7cdca9ae3a0dbf6442
ashwani436 <sharmagpl786@gmail.com>
2021-04-30 21:38:30 +0530

    save changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import PlanetSearch from './components/PlanetSearch';
+import Vehiclesearch from './components/Vehiclesearch';
+
+function App() {
+  return (
+    <>
+    <PlanetSearch/>
+    <Vehiclesearch/>
+    </>
+
+  );
+}
+
+export default App;
