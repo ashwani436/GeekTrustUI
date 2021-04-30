eb6a4a11bad057451d43793f2a4044a7768b6745
ashwani436 <sharmagpl786@gmail.com>
2021-04-30 21:50:54 +0530

    save changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Vehiclesearch from './components/Vehiclesearch';
 function App() {
   return (
     <>
+    <h1>Finding Falcon</h1>
+    <br/>
+    <br/>
+    <h3>Select Planet You want to Search in!</h3>
     <PlanetSearch/>
     <Vehiclesearch/>
     </>
